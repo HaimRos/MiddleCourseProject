03949e0163e3cd9ff206f90054f1b8215c0e6eb2
Haim <haimros@gmail.com>
2018-06-11 10:59:54 +0300

    adding js for cjanging 3 dots src image to blue while mouse hover

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeBgHoverOn(id){
+    document.getElementById(id).src = "images/read_more_blue.png";
+}
+
+function changeBgHoverOff(id){
+    document.getElementById(id).src = "images/read_more_white.png";
+}
